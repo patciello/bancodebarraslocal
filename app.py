3d2f2f57723cd3a0373d09d1085f68341fb79553
Leonardo Paciello Ferreira Dias <leonardopaciellop@gmail.com>
2024-09-20 22:49:16 -0300

    Google Auth Implement

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ import os
 from dotenv import load_dotenv
 
 #Google OAuth
-# from authlib.integrations.flask_client import OAuth
+from authlib.integrations.flask_client import OAuth
+
+
+# Carregando as credenciais do arquivo JSON
+with open('config/credentials.json') as f:
+    credentials = json.load(f)
+
 
 
 app = Flask(__name__)
@@ -12,55 +18,55 @@ app = Flask(__name__)
 # Load environment variables
 load_dotenv()
 app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
-app.secret_key = os.environ.get('SECRET_KEY')
+app.secret_key = os.environ.get('SECRET_KEY', 'minha_chave_secreta')
 
 # Initialize MongoDB client
 client = MongoClient(app.config['MONGO_URI'])
 db = client['barcode_db']
 collection = db['barcodes']
 
-# Configuração do OAuth
-# oauth = OAuth(app)
-# oauth.register(
-#     name='google',
-#     client_id=os.environ.get('GOOGLE_CLIENT_ID'),
-#     client_secret=os.environ.get('GOOGLE_CLIENT_SECRET'),
-#     access_token_url='https://accounts.google.com/o/oauth2/token',
-#     authorize_url='https://accounts.google.com/o/oauth2/auth',
-#     authorize_params=None,
-#     access_token_params=None,
-#     authorize_redirect_uri='https://bancodebarraslocal.onrender.com',
-#     api_base_url='https://www.googleapis.com/oauth2/v1/',
-#     client_kwargs={'scope': 'openid profile email'}
-# )
+
+# Configuração do OAuth usando as credenciais do JSON
+oauth = OAuth(app)
+oauth.register(
+    name='google',
+    client_id=credentials['web']['client_id'],
+    client_secret=credentials['web']['client_secret'],
+    access_token_url=credentials['web']['token_uri'],
+    authorize_url=credentials['web']['auth_uri'],
+    authorize_redirect_uri=credentials['web']['redirect_uris'][0],  # Usando o primeiro redirect_uri da lista
+    api_base_url='https://www.googleapis.com/oauth2/v1/',
+    client_kwargs={'scope': 'openid profile email'}
+)
 
 
 
-# @app.route('/')
-# def index():
-#     if 'google_token' in session:
-#         user = oauth.google.get('userinfo').json()
-#         return f"Você está logado como {user['email']}"
-#     return redirect(url_for('login'))
 
-# @app.route('/login')
-# def login():
-#     redirect_uri = url_for('auth', _external=True)
-#     return oauth.google.authorize_redirect(redirect_uri)
+@app.route('/')
+def index():
+    if 'google_token' in session:
+        user = oauth.google.get('userinfo').json()
+        return f"Você está logado como {user['email']}"
+    return redirect(url_for('login'))
 
-# @app.route('/auth')
-# def auth():
-#     token = oauth.google.authorize_access_token()
-#     session['google_token'] = token
-#     user = oauth.google.get('userinfo').json()
-#     session['user_info'] = user
-#     return redirect('/')
+@app.route('/login')
+def login():
+    redirect_uri = url_for('auth', _external=True)
+    return oauth.google.authorize_redirect(redirect_uri)
 
-# @app.route('/logout')
-# def logout():
-#     session.pop('google_token', None)
-#     session.pop('user_info', None)
-#     return redirect('/')
+@app.route('/auth')
+def auth():
+    token = oauth.google.authorize_access_token()
+    session['google_token'] = token
+    user = oauth.google.get('userinfo').json()
+    session['user_info'] = user
+    return redirect('/')
+
+@app.route('/logout')
+def logout():
+    session.pop('google_token', None)
+    session.pop('user_info', None)
+    return redirect('/')
 
 
 
