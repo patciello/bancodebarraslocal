65b3984cd5387db2cf33cddd19c2286156c6ea12
Leonardo Paciello Ferreira Dias <leonardopaciellop@gmail.com>
2024-09-20 22:59:13 -0300

    Google Auth adjust

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
@@ -39,39 +39,11 @@ oauth.register(
     client_kwargs={'scope': 'openid profile email'}
 )
 
-
-
-
-@app.route('/')
-def index():
-    if 'google_token' in session:
-        user = oauth.google.get('userinfo').json()
-        return f"Você está logado como {user['email']}"
-    return redirect(url_for('login'))
-
-@app.route('/login')
-def login():
-    redirect_uri = url_for('auth', _external=True)
-    return oauth.google.authorize_redirect(redirect_uri)
-
-@app.route('/auth')
-def auth():
-    token = oauth.google.authorize_access_token()
-    session['google_token'] = token
-    user = oauth.google.get('userinfo').json()
-    session['user_info'] = user
-    return redirect('/')
-
-@app.route('/logout')
-def logout():
-    session.pop('google_token', None)
-    session.pop('user_info', None)
-    return redirect('/')
-
-
-
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    if 'google_token' not in session:
+        return redirect(url_for('login'))
+    
     if request.method == 'POST':
         barcode = request.form.get('barcode')
         if collection.find_one({'barcode': barcode}):
@@ -80,14 +52,43 @@ def home():
             collection.insert_one({'barcode': barcode})
             flash('Código de barras adicionado com sucesso!', 'success')
         return redirect(url_for('home'))
-    # Retrieve the last 10 barcodes
+    
+    # Recupera os últimos 10 códigos de barras
     recent_barcodes = collection.find().sort('_id', -1).limit(10)
     return render_template('home.html', barcodes=recent_barcodes)
 
+# Rota para visualizar o histórico de códigos de barras
 @app.route('/history')
 def history():
+    if 'google_token' not in session:
+        return redirect(url_for('login'))
+    
     all_barcodes = collection.find().sort('_id', -1)
     return render_template('history.html', barcodes=all_barcodes)
+
+# Rota de login
+@app.route('/login')
+def login():
+    redirect_uri = url_for('auth', _external=True)
+    return oauth.google.authorize_redirect(redirect_uri)
+
+# Rota de autenticação
+@app.route('/auth')
+def auth():
+    token = oauth.google.authorize_access_token()
+    session['google_token'] = token
+    user = oauth.google.get('userinfo').json()
+    session['user_info'] = user
+    return redirect(url_for('home'))
+
+# Rota de logout
+@app.route('/logout')
+def logout():
+    session.pop('google_token', None)
+    session.pop('user_info', None)
+    return redirect(url_for('home'))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
