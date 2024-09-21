7d12f35fba0e90af315045a5c2809ca23883d91a
leonardopaciellop@gmail.com <leonardopaciellop@gmail.com>
2024-09-21 07:41:29 -0300

    Config OAuth - Landim PC

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 # Load environment variables
 load_dotenv()
 app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
-app.secret_key = os.environ.get('SECRET_KEY', 'minha_chave_secreta')
+app.secret_key = os.environ.get('SECRET_KEY', 'CLIENT_SECRET')
 
 # Initialize MongoDB client
 client = MongoClient(app.config['MONGO_URI'])
@@ -26,11 +26,11 @@ collection = db['barcodes']
 oauth = OAuth(app)
 oauth.register(
     name='google',
-    client_id=credentials('CLIENT_ID'),
-    client_secret=credentials('CLIENT_SECRET'),
-    access_token_url=credentials('GOOGLE_TOKEN_URI'),
-    authorize_url=credentials('GOOGLE_AUTH_URI'),
-    authorize_redirect_uri=credentials('GOOGLE_REDIRECT_URI'),  # Usando o primeiro redirect_uri da lista
+    client_id=os.environ.get('GOOGLE_CLIENT_ID'),
+    client_secret=os.environ.get('GOOGLE_CLIENT_SECRET'),
+    access_token_url=os.environ.get('GOOGLE_TOKEN_URI'),
+    authorize_url=os.environ.get('GOOGLE_AUTH_URI'),
+    authorize_redirect_uri=os.environ.get('GOOGLE_REDIRECT_URI'),
     api_base_url='https://www.googleapis.com/oauth2/v1/',
     client_kwargs={'scope': 'openid profile email'}
 )
