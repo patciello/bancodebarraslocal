a033fa9c10098ae33046e659c4b992c68b199f60
leonardopaciellop@gmail.com <leonardopaciellop@gmail.com>
2024-10-23 13:06:12 -0300

    Upate - change date/hour, nfe number

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 
-# Google OAuth
-from authlib.integrations.flask_client import OAuth
-
 app = Flask(__name__)
 
 # Load environment variables
@@ -19,24 +16,8 @@ client = MongoClient(app.config['MONGO_URI'])
 db = client['barcode_db']
 collection = db['barcodes']
 
-# Configuração do OAuth usando as credenciais do JSON
-oauth = OAuth(app)
-oauth.register(
-    name='google',
-    client_id=os.environ.get('GOOGLE_CLIENT_ID'),
-    client_secret=os.environ.get('GOOGLE_CLIENT_SECRET'),
-    access_token_url=os.environ.get('GOOGLE_TOKEN_URI'),
-    authorize_url=os.environ.get('GOOGLE_AUTH_URI'),
-    authorize_redirect_uri=os.environ.get('GOOGLE_REDIRECT_URI'),
-    api_base_url='https://www.googleapis.com/oauth2/v1/',
-    client_kwargs={'scope': 'openid profile email'}
-)
-
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if 'google_token' not in session:
-        return redirect(url_for('login'))
-    
     if request.method == 'POST':
         barcode = request.form.get('barcode')
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -62,34 +43,9 @@ def home():
 # Rota para visualizar o histórico de códigos de barras
 @app.route('/history')
 def history():
-    if 'google_token' not in session:
-        return redirect(url_for('login'))
-    
     all_barcodes = collection.find().sort('_id', -1)
     all_barcodes = [{'barcode': b['barcode'], 'timestamp': b['timestamp'], 'trecho': b.get('trecho')} for b in all_barcodes]
     return render_template('history.html', barcodes=all_barcodes)
-
-# Rota de login
-@app.route('/login')
-def login():
-    redirect_uri = url_for('auth', _external=True)
-    return oauth.google.authorize_redirect(redirect_uri)
-
-# Rota de autenticação
-@app.route('/auth')
-def auth():
-    token = oauth.google.authorize_access_token()
-    session['google_token'] = token
-    user = oauth.google.get('userinfo').json()
-    session['user_info'] = user
-    return redirect(url_for('home'))
-
-# Rota de logout
-@app.route('/logout')
-def logout():
-    session.pop('google_token', None)
-    session.pop('user_info', None)
-    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
