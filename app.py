bced896c556e0b49465af1c02e51cfd5a3e76952
leonardopaciellop@gmail.com <leonardopaciellop@gmail.com>
2024-10-23 14:26:49 -0300

    Remove  - Google Auth

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash
 from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
